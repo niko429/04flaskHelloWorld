c5c848bf59d08111cfa0ea590618e7ccb99b1e37
Niko985642 <tomjeryy16@gmail.com>
2026-09-24 20:25:04 +0200

    Dodano drugą trasę o nazwie kontakt

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ def index():
 def onas():
     return "Nazywam się nikodem naperty, lubię jeść pizzę i nie jestem winny kartosowi pizzy."
 
+@app.route("/kontakt")
+def kont():
+    return "eeeee, nie mam zbytnio kontaktów"
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
